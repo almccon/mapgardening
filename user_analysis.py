804e1fadef7c3b9a0ca1822b8bb673a4de07df24
Alan McConchie <alan.mcconchie@gmail.com>
2013-03-31 22:15:43 -0700

    Adding a script to run user-based stats

---
+++ user_analysis.py
@@ -0,0 +1,56 @@
+#! /usr/bin/env python
+
+import MapGardening
+from MapGardening import UserStats
+import time
+import optparse
+
+
+usage = "usage: %prog [options]"
+p = optparse.OptionParser(usage)
+p.add_option('--place', '-p',
+             default="all"
+             )
+
+options, arguments = p.parse_args()
+
+if options.place == "all":
+
+    places = MapGardening.get_all_places()
+
+else:
+   
+    placename = options.place 
+    place = MapGardening.get_place(placename)
+    places = {placename: place}
+
+MapGardening.init_logging()
+
+for placename in places.keys():
+    
+    st = time.time()
+    lt = st
+    
+    print "starting", placename 
+       
+    (conn, cur) = MapGardening.init_db(places[placename]['dbname'])
+    us = UserStats.UserStats(conn, cur)
+        
+#   create_userstats_table()
+       
+#   add_userstats_blankedits() 
+#   add_userstats_v1edits() 
+
+    us.add_userstats_firstedit()
+    us.add_userstats_firstedit_v1()
+    us.add_userstats_firstedit_blank()
+
+    us.print_userstats("output2_" + placename + ".tsv")
+            
+
+             
+    now = time.time()
+    print "finished. time elapsed: {:.0f} sec, {:.0f} sec total".format(now - lt, now - st)
+    lt = now
+    
+    
