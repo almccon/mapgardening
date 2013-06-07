71aa7050d5c6995b39df6cd533b29412825a5a04
Alan McConchie <alan.mcconchie@gmail.com>
2013-06-06 22:57:52 -0700

    Separating the timeseries charts into another file

--- scatter.R
+++ scatter.R
@@ -73,27 +73,4 @@ for (place in places) {
 
     detach(frame)
 
-    inputfile = paste0('/Users/alan/github/mapgardening/output_totals_', place, '_raster_', scale, '.tsv')
-
-    classes <- c("Date","numeric","numeric","numeric")
-
-    frame2 <- read.table(inputfile, header=TRUE, colClasses=classes)
-
-    names(frame2) = c("date","edits","v1edits","blankedits")
-
-    attach(frame2)
-
-    outputfile = paste0('/Users/alan/github/mapgardening/output_history_', place, '_', scale, '.png')
-    png(outputfile, 600, 600)
-
-    a <- ggplot(data = frame2, aes(x = date, y = cumsum(blankedits))) +
-      geom_line() +
-      scale_x_date("Date") + scale_y_continuous("Cumulative blankspot edits") +
-      ggtitle(paste0(place, '_', scale))
-    print(a)
-    dev.off()
-
-
-    detach(frame2)
-  }
 }
